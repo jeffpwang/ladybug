2a33f37fe237dc2d97d172cbcd1184bd46def412
mallorycoppenrath <malcoppenrath@gmail.com>
2016-03-15 18:15:02 -0400

    form work

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -5,12 +5,18 @@ def new
 end
 
 def create
- @log = Log.new(logs_params)
+ @log = Log.create(logs_params)
  if @log.valid?
   @log.save
+
+  redirect_to @log
  else
   render 'new'
  end
+end
+
+def show
+  @log = Log.find(params[:id])
 end
 
 private
