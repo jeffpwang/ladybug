e52b6b65a6cc00a193a0fdf31844ef3306b7f5f7
itsatype <leaheinhorn@yahoo.com>
2016-03-15 15:44:25 -0400

    seed file revision

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
 
      has_many :logs
      has_many :ladybugs, through: :logs
      has_many :distortions, through: :logs
      has_many :beliefs, through: :logs
+     has_many :tags, through: :logs
 end
