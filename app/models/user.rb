3ceeac22a6e6c637d5eb0e67cdce4c0ccf4eb470
itsatype <leaheinhorn@yahoo.com>
2016-03-15 15:14:43 -0400

    add relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :logs
+     has_many :logs
+     has_many :ladybugs, through: :logs
+     has_many :distortions, through: :logs
+     has_many :beliefs, through: :logs
 end
