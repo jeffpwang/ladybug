2775718da4ef21655d4ef24791608d6120e275ee
itsatype <leaheinhorn@yahoo.com>
2016-03-16 14:29:58 -0400

    add user show page

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,14 +1,13 @@
 class LogsController < ApplicationController
 
 def new
- @log = Log.new
+	@log = current_user.logs.build 
 end
 
 def create
- @log = Log.create(logs_params)
+ @log =  current_user.logs.build(logs_params)
  if @log.valid?
   @log.save
-
   redirect_to @log
  else
   render 'new'
@@ -22,7 +21,7 @@ end
 private
 
 def logs_params
- params.require(:log).permit(:content, :before_rating, :after_rating)
+ params.require(:log).permit(:content, :before_rating, :after_rating, :belief_id, :distortion_id, :lady_bug_ids, :tag_ids => [])
 end
 
 end
