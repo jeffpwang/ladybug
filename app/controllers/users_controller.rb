b7640cbd70fb18ecc7d829daa9f6a5f5b5219f55
mallorycoppenrath <malcoppenrath@gmail.com>
2016-03-18 11:56:59 -0400

    pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@logs = Log.paginate(:per_page => 2, :page => params[:page])
+		@logs = @user.logs.paginate(:page => params[:page], :per_page => 3)
 	end
 
 	def delete
