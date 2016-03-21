ed4293472cd490c7b40e139cad4291a6510deb34
mallorycoppenrath <malcoppenrath@gmail.com>
2016-03-20 20:18:36 -0400

    conflict solved

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,9 +1,21 @@
-require 'spec_helper'
-require 'rails_helper'
 require 'feature_helper'
 
- 	describe 'validations' do
-			user = Factory(:user)		
+describe "creating users" do
+  context "on the new user page" do
+    before do
+      @user = FactoryGirl.create(:user)
+      page.set_rack_session(:user_id => @user.id)  
+      visit new_user_path
+    end
+  end     
+    it "should have a form to create the user" do
+      expect(page).to have_css("form#new_user")
+      binding.pry
+      fill_in 'user_name', with: 'Tommy'
+      click_button('Create User')
+      expect(User.first.name).to eq("Tommy")      
+      expect(page).to have_name("Tommy")
+    end    
 
 
 end
